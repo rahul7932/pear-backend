a70842769398fa9110dc96a9c9d6714c0a110e12
Varun Kandula <kandula.varun@gmail.com>
2024-10-11 23:48:32 -0700

    supaasbe setup

--- controllers/db_controller.py
+++ controllers/db_controller.py
@@ -1,6 +1,16 @@
 from supabase import create_client, Client
-from dotenv import load_dotenv
+from supabase.storage import StorageFileAPI
 
-load_dotenv()
+# from dotenv import load_dotenv
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+# load_dotenv()
+
+SUPABASE_URL = "https://zmihpyxsmvcstuyukgly.supabase.co"
+SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptaWhweXhzbXZjc3R1eXVrZ2x5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2MjY0NzIsImV4cCI6MjA0NDIwMjQ3Mn0.FVBM2zKO-UjQW_Pz2mtNguRr4AeGj3c7r0NXLe0ZxWU"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+
+storage: StorageFileAPI = supabase.storage()
+files = storage.from_("test_bucket").list()
+print(files)
